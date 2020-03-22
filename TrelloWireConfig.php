ab41992bebbde0568d786c5d5afc4483dd8fc1da
Moritz L'Hoest <info@herebedragons.world>
2020-03-22 18:51:34 +0100

    Build options for status changes, update existing cards when a page is saved

--- TrelloWireConfig.php
+++ TrelloWireConfig.php
@@ -3,6 +3,7 @@ namespace Processwire;
 
 use ProcessWire\Inputfield;
 use ProcessWire\InputfieldAsmSelect;
+use ProcessWire\TrelloWire;
 use ProcessWire\TrelloWireApi;
 use Throwable;
 
@@ -13,11 +14,24 @@ class TrelloWireConfig extends ModuleConfig
         return [
             'ApiKey' => '',
             'ApiToken' => '',
-            'CardPageSelector' => '',
+            'TargetBoard' => '',
+            'TargetList' => '',
+            'TrelloWireTemplates' => [],
             'CardTitle' => 'title',
             'CardBody' => '',
-            // @TODO: ADD ALL OPTIONS AS DEFAULT!
             'CardCreationTrigger' => TrelloWire::CREATE_ON_PUBLISHED,
+            'CardUpdate' => true,
+            'StatusChangeHidden' => TrelloWire::STATUS_CHANGE_NO_ACTION,
+            'MoveListTargetHidden' => '',
+            'RestoreOnReverseHidden' => false,
+            'StatusChangeUnpublished' => TrelloWire::STATUS_CHANGE_NO_ACTION,
+            'MoveListTargetUnpublished' => '',
+            'RestoreOnReverseUnpublished' => false,
+            'StatusChangeTrashed' => TrelloWire::STATUS_CHANGE_ARCHIVE,
+            'MoveListTargetTrashed' => '',
+            'RestoreOnReverseTrashed' => true,
+            'StatusChangeDeleted' => TrelloWire::STATUS_CHANGE_DELETE,
+            'MoveListTargetDeleted' => '',
         ];
     }
 
@@ -35,18 +49,17 @@ class TrelloWireConfig extends ModuleConfig
         $ApiKey = wire()->modules->get('InputfieldText');
         $ApiKey->name = 'ApiKey';
         $ApiKey->label = $this->_('Trello API key');
-        $ApiKey->required = true;
-        $ApiKey->description = sprintf($this->_('You can [generate your API key here](%s).'), 'https://trello.com/app-key');
         $ApiKey->columnWidth = 50;
         $ApiKey->collapsed = Inputfield::collapsedNever;
+        $ApiKey->required = true;
+        $ApiKey->description = sprintf($this->_('You can [generate your API key here](%s).'), 'https://trello.com/app-key');
 
         $ApiToken = wire()->modules->get('InputfieldText');
         $ApiToken->name = 'ApiToken';
         $ApiToken->label = $this->_('Trello API token');
+        $ApiToken->columnWidth = 50;
+        $ApiToken->collapsed = Inputfield::collapsedNever;
         $ApiToken->required = $hasApiKey;
-        if (!$hasApiKey) {
-            $this->disableField($ApiToken);
-        }
         $tokenFieldDescription = $hasApiKey
             ? $this->_('[Generate your access token using this link](%s).')
             : $this->_('Please set your API key first.');
@@ -59,14 +72,15 @@ class TrelloWireConfig extends ModuleConfig
                 $currentApiKey
             )
         );
+        if (!$hasApiKey) {
+            $this->disableField($ApiToken);
+        }
         if ($hasInvalidToken) {
             $ApiToken->error($this->_('The API token is invalid! It may have been deleted, expired or been revoked.'));
         }
         if ($hasApiToken && !$hasInvalidToken) {
             $ApiToken->notes($this->_('SUCCESS: API token apears to be working!'));
         }
-        $ApiToken->columnWidth = 50;
-        $ApiToken->collapsed = Inputfield::collapsedNever;
 
         $inputfields->add($ApiKey);
         $inputfields->add($ApiToken);
@@ -74,6 +88,12 @@ class TrelloWireConfig extends ModuleConfig
         // @TODO: Add usage notes / descriptions to all fields
         if ($hasApiKey && $hasApiToken && !$hasInvalidToken) {
             $TrelloWireApi = $TrelloWire->api();
+
+            try {
+                $availableLists = $TrelloWireApi->lists($TrelloWire->TargetBoard);
+            } catch (Throwable $e) {
+                $availableLists = null;
+            }
 
             $TargetBoard = wire()->modules->get('InputfieldSelect');
             $TargetBoard->name = 'TargetBoard';
@@ -96,10 +116,9 @@ class TrelloWireConfig extends ModuleConfig
             if (!$TrelloWire->TargetBoard) {
                 $this->disableField($TargetList);
             } else {
-                try {
-                    $listsInBoard = $TrelloWireApi->lists($TrelloWire->TargetBoard);
-                    $TargetList->setOptions(array_combine(array_column($listsInBoard, 'id'), array_column($listsInBoard, 'name')));
-                } catch (Throwable $e) {
+                if (null !== $availableLists) {
+                    $TargetList->setOptions(array_combine(array_column($availableLists, 'id'), array_column($availableLists, 'name')));
+                } else {
                     $TargetList->error($this->_('Error retrieving lists for this board. The board may have been deleted.'));
                 }
             }
@@ -143,17 +162,69 @@ class TrelloWireConfig extends ModuleConfig
                 TrelloWire::CREATE_ON_ADDED => $this->_('Create new cards whenever an applicable page is added'),
                 TrelloWire::CREATE_ON_PUBLISHED => $this->_('Create new cards whenever an applicable page is published'),
             ]);
-            $CardCreationTrigger->columnWidth = 34;
+            $CardCreationTrigger->columnWidth = 50;
             $CardCreationTrigger->collapsed = Inputfield::collapsedNever;
+
+            $CardUpdate = wire()->modules->get('InputfieldCheckbox');
+            $CardUpdate->name = 'CardUpdate';
+            $CardUpdate->label = $this->_('Page update handling');
+            $CardUpdate->label2 = $this->_('Update Trello cards when pages are updated');
+            $CardUpdate->description = $this->_('When a page with a reference to a card is saved, update the card on Trello?');
+            $CardUpdate->columnWidth = 50;
+            $CardUpdate->collapsed = Inputfield::collapsedNever;
 
             $CardSyncSettings = wire()->modules->get('InputfieldFieldset');
             $CardSyncSettings->label = $this->_('Card creation and update settings');
             $CardSyncSettings->collapsed = Inputfield::collapsedNo;
 
             $CardSyncSettings->add($CardCreationTrigger);
+            $CardSyncSettings->add($CardUpdate);
+
+            $StatusChanges = wire()->modules->get('InputfieldFieldset');
+            $StatusChanges->label = $this->_('Status change handling');
+            $StatusChanges->collapsed = Inputfield::collapsedNo;
+
+            $StatusChangeLabels = $this->statusChangeFieldLabels();
+            foreach (['Hidden', 'Unpublished', 'Trashed', 'Deleted'] as $status) {
+                $fieldName = "StatusChange{$status}";
+                $StatusChangeOptions = wire()->modules->get('InputfieldRadios');
+                $StatusChangeOptions->name = $fieldName;
+                $StatusChangeOptions->label = $StatusChangeLabels[$status]['label'];
+                $StatusChangeOptions->addOptions([
+                    TrelloWire::STATUS_CHANGE_NO_ACTION => $this->_('Do nothing'),
+                    TrelloWire::STATUS_CHANGE_MOVE => $this->_('Move the card to a different list'),
+                    TrelloWire::STATUS_CHANGE_ARCHIVE => $this->_('Archive the card'),
+                    TrelloWire::STATUS_CHANGE_DELETE => $this->_('Delete the card (irreversibly)'),
+                ]);
+                $StatusChangeOptions->required = true;
+                $StatusChangeOptions->columnWidth = 34;
+                $StatusChangeOptions->collapsed = Inputfield::collapsedNever;
+                $StatusChanges->add($StatusChangeOptions);
+
+                $MoveListTarget = wire()->modules->get('InputfieldSelect');
+                $MoveListTarget->name = "MoveListTarget{$status}";
+                $MoveListTarget->label = $this->_('Select the list to move the card to');
+                $MoveListTarget->columnWidth = 33;
+                $MoveListTarget->collapsed = Inputfield::collapsedNever;
+                $MoveListTarget->setOptions(array_combine(array_column($availableLists, 'id'), array_column($availableLists, 'name')));
+                $MoveListTarget->showIf(sprintf('%s=%s', $fieldName, TrelloWire::STATUS_CHANGE_MOVE));
+                $MoveListTarget->requiredIf(sprintf('%s=%s', $fieldName, TrelloWire::STATUS_CHANGE_MOVE));
+                $StatusChanges->add($MoveListTarget);
+
+                if (!in_array($status, ['Deleted'])) {
+                    $RestoreOnReverse = wire()->modules->get('InputfieldCheckbox');
+                    $RestoreOnReverse->name = "RestoreOnReverse{$status}";
+                    $RestoreOnReverse->label = $StatusChangeLabels[$status]['restoreLabel'];
+                    $RestoreOnReverse->columnWidth = 33;
+                    $RestoreOnReverse->collapsed = Inputfield::collapsedNever;
+                    $RestoreOnReverse->showIf(sprintf('%s=%s', $fieldName, TrelloWire::STATUS_CHANGE_ARCHIVE));
+                    $StatusChanges->add($RestoreOnReverse);
+                }
+            }
 
             $inputfields->add($TrelloTargetSettings);
             $inputfields->add($CardSyncSettings);
+            $inputfields->add($StatusChanges);
         }
 
         return $inputfields;
@@ -173,5 +244,26 @@ class TrelloWireConfig extends ModuleConfig
         // @TODO: cleaner "disabled" status & styling
         $inputfield->attr('disabled', 'disabled');
         $inputfield->attr('style', 'background: #ccc; cursor: not-allowed;');
+    }
+
+    protected function statusChangeFieldLabels(): array
+    {
+        return [
+            'Hidden' => [
+                'label' => $this->_('What should happen if the page is hidden?'),
+                'restoreLabel' => $this->_('Restore the card when the page is unhidden?'),
+            ],
+            'Unpublished' => [
+                'label' => $this->_('What should happen if the page is unpublished?'),
+                'restoreLabel' => $this->_('Restore the card when the page is published again?'),
+            ],
+            'Trashed' => [
+                'label' => $this->_('What should happen if the page is trashed?'),
+                'restoreLabel' => $this->_('Restore the card when the page is restored from the trash?'),
+            ],
+            'Deleted' => [
+                'label' => $this->_('What should happen if the page is deleted?'),
+            ],
+        ];
     }
 }
--- src/TrelloWireApi.php
+++ src/TrelloWireApi.php
@@ -6,40 +6,90 @@ use ProcessWire\WireHttp;
 
 class TrelloWireApi
 {
+    /** @var string The base URL for the Trello API. */
     public const API_BASE = 'https://api.trello.com/1/';
 
-    protected $ApiKey;
-    protected $ApiToken;
-
+    /** @var WireHttp Always contains the WireHttp instance used for the last request made through this object. */
     public $lastRequest;
 
+    /** @var string The API key for Trello. */
+    protected $ApiKey;
+
+    /** @var string The API token for Trello. */
+    protected $ApiToken;
+
+    /**
+     * Construct a new API instance with an API key & token.
+     *
+     * @param string $ApiKey
+     * @param string $ApiToken
+     */
     public function __construct(string $ApiKey, string $ApiToken)
     {
         $this->ApiKey = $ApiKey;
         $this->ApiToken = $ApiToken;
     }
 
+    /**
+     * Send a GET request to the specified endpoint of the Trello API.
+     *
+     * @param string $endpoint  The endpoint to call, including relevant parameters, without a leading slash.
+     * @param array $data       Associative array of parameters to add to this request.
+     * @return mixed
+     */
     public function get(string $endpoint, array $data = [])
     {
         return $this->send($endpoint, 'GET', $data);
     }
 
+    /**
+     * Send a POST request to the specified endpoint of the Trello API.
+     *
+     * @param string $endpoint  The endpoint to post to, including relevant parameters, without a leading slash.
+     * @param array $data       Associative array of data to send with this request.
+     * @return mixed
+     */
     public function post(string $endpoint, array $data = [])
     {
         return $this->send($endpoint, 'POST', $data);
     }
 
+    /**
+     * Send a PUT request to the specified endpoint of the Trello API.
+     *
+     * @param string $endpoint  The endpoint to call, including relevant parameters, without a leading slash.
+     * @param array $data       Associative array of data to send with this request.
+     * @return mixed
+     */
     public function put(string $endpoint, array $data = [])
     {
         return $this->send($endpoint, 'PUT', $data);
     }
 
+    /**
+     * Send a DELETE request to the specified endpoint of the Trello API.
+     *
+     * @param string $endpoint  The endpoint to call, including relevant parameters, without a leading slash.
+     * @param array $data       Associative array of data to send with this request.
+     * @return mixed
+     */
     public function delete(string $endpoint, array $data = [])
     {
         return $this->send($endpoint, 'DELETE', $data);
     }
 
-    protected function send(string $endpoint, string $method = 'GET', array $data = [])
+    /**
+     * Send a request to any endpoint of the Trello API. This passes requests to
+     * the appropriate method of ProcessWire's WireHttp class, so check the
+     * documentation for possible return values.
+     *
+     * @param string $endpoint  The endpoint to call, including relevant parameters, without a leading slash.
+     * @param string $method    The HTTP method to use (one of GET, POST, PUT, DELETE).
+     * @param array $data       Additional data to send with this request.
+     * @see https://processwire.com/api/ref/wire-http/
+     * @return mixed
+     */
+    public function send(string $endpoint, string $method = 'GET', array $data = [])
     {
         $WireHttp = new WireHttp();
         $this->lastRequest = $WireHttp;
@@ -61,6 +111,11 @@ class TrelloWireApi
         }
     }
 
+    /**
+     * Check if the API token this instance is configured to use is valid.
+     *
+     * @return boolean
+     */
     public function isValidToken(): bool
     {
         $this->get(sprintf('tokens/%s?fields=dateExpires&webhooks=false', $this->ApiToken));
@@ -68,10 +123,20 @@ class TrelloWireApi
         return $httpResponseCode >= 200 & $httpResponseCode < 300;
     }
 
-    public function boards(): array
+    /**
+     * Get all boards belonging to the user that created the current API token.
+     * 
+     * @param array $fields     Board fields to include in the response.
+     * @param string $filter    The filter to use for this request.
+     * @return array
+     */
+    public function boards(array $fields = ['id', 'name'], string $filter = 'all'): array
     {
-        // @TODO: add params: fields, filter
-        $result = $this->get('members/me/boards?fields=id,name,idOrganization&filter=open');
+        $result = $this->get(sprintf(
+            'members/me/boards?fields=%1$s&filter=%2$s',
+            implode(',', $fields),
+            $filter
+        ));
         $responseCode = $this->lastRequest->getHttpCode();
         if (!$this->isResponseCodeOk($responseCode)) {
             throw new \InvalidArgumentException(sprintf($this->_('Error retrieving Trello boards. HTTP Code: %d'), $responseCode));
@@ -79,19 +144,44 @@ class TrelloWireApi
         return json_decode($result);
     }
 
-    public function lists(string $idBoard): array
+    /**
+     * Get the lists in a board.
+     *
+     * @param string $idBoard   The ID of the board.
+     * @param array $fields     The board fields to include in the response.
+     * @param string $filter    The filter to use for this request.
+     * @param string $cards     The type of cards to include in the response.
+     * @param array $cardFields The card fields to include in the response.
+     * @return array
+     */
+    public function lists(string $idBoard, array $fields = ['id', 'name', 'pos'], string $filter = 'open', string $cards = 'none', array $cardFields = []): array
     {
-        // @TODO: add params: fields, filter
-        $result = $this->get("boards/${idBoard}/lists?cards=none&filter=open&fields=id,name,pos");
+        $result = $this->get(sprintf(
+            'boards/%1$s/lists?fields=%2$s&filter=%3$s&cards=%4$s&card_fields=%5$s',
+            $idBoard,
+            implode(',', $fields),
+            $filter,
+            $cards,
+            implode(',', $cardFields)
+        ));
         if (!$this->isResponseCodeOk($this->lastRequest->getHttpCode())) {
             throw new \InvalidArgumentException(sprintf($this->_('Board with ID %s does not appear to exist.'), $idBoard));
         }
         return json_decode($result);
     }
 
-    public function postCard(string $idList, string $title, string $body = '', array $addData = []): bool
+    /**
+     * Post a new card to the specified list. Returns true on success, false on failure.
+     *
+     * @param string $idList    The ID of the list to add the card to.
+     * @param string $title     The name / title of the card.
+     * @param string $body      The description / body of the card.
+     * @param array $addData    Additional fields to pass to the API.
+     * @return boolean
+     */
+    public function postCard(string $idList, string $title, string $body = '', array $addData = [])
     {
-        $this->post('cards', array_merge(
+        $result = $this->post('cards', array_merge(
             $addData,
             [
                 'name' => $title,
@@ -99,9 +189,25 @@ class TrelloWireApi
                 'idList' => $idList
             ]
         ));
-        return $this->isResponseCodeOk($this->lastRequest->getHttpCode());
+        return $this->isResponseCodeOk($this->lastRequest->getHttpCode()) ? json_decode($result) : false;
     }
 
+    public function updateCard(string $id, ?string $title = null, ?string $body = null, array $addData = [])
+    {
+        $updateFields = array_merge($addData, array_filter([
+            'name' => $title,
+            'desc' => $body,
+        ]));
+        $result = $this->put(sprintf('cards/%s', $id), $updateFields);
+        return $this->isResponseCodeOk($this->lastRequest->getHttpCode()) ? json_decode($result) : false;
+    }
+
+    /**
+     * Check if an HTTP response code is in the OK range (2XX).
+     *
+     * @param integer $httpCode
+     * @return boolean
+     */
     public function isResponseCodeOk(int $httpCode): bool
     {
         return $httpCode >= 200 && $httpCode < 300;
